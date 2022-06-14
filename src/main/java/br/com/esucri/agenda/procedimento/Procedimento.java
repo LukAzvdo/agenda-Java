30ad592326c44967818de040c43122ef717f9008
Luciano de Azevedo <luk.azvdo@gmail.com>
2022-06-14 20:14:46 -0300

    Init Git

---
+++ src/main/java/br/com/esucri/agenda/procedimento/Procedimento.java
@@ -0,0 +1,51 @@
+/*
+ */
+package br.com.esucri.agenda.procedimento;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "procedimentos", schema = "public")
+public class Procedimento implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "CODIGO",length = 8,nullable = false, unique = true)
+    private int codigo;
+    
+    @Column(name = "DESCRICAO",length = 100,nullable = false, unique = true)
+    private String descricao;
+
+    public Procedimento() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao.trim().isEmpty() ? "PROCEDIMENTO PADRÃO" :descricao.toUpperCase();
+    }
+}
